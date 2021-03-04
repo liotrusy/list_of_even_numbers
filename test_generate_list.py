587343c5dd9ee4fa377ab19b139094ba2a432542
liotrusy <pierre.collura1989@gmail.com>
2021-03-04 19:21:18 +0100

    completed tests for hard-coded version

--- test_generate_list.py
+++ test_generate_list.py
@@ -1,14 +1,16 @@
 import pytest
-import generate_list
+import generate_list_hardcoded as hard_coded
 
-def test_generate_list_version_1():
-    assert generate_list.generate_list_version_1() == [2, 4, 6, 8, 10]
+class TestGenerateListHardCoded:
 
-def test_generate_list_version_2():
-    assert generate_list.generate_list_version_2() == [2, 4, 6, 8, 10]
+    def test_generate_list_version_1(self):
+        assert hard_coded.generate_list_version_1() == [2, 4, 6, 8, 10]
 
-def test_generate_list_version_3():
-    assert generate_list.generate_list_version_3() == [2, 4, 6, 8, 10]
+    def test_generate_list_version_2(self):
+        assert hard_coded.generate_list_version_2() == [2, 4, 6, 8, 10]
 
-def test_generate_list_version_4():
-    assert generate_list.generate_list_version_4() == [2, 4, 6, 8, 10]
+    def test_generate_list_version_3(self):
+        assert hard_coded.generate_list_version_3() == [2, 4, 6, 8, 10]
+
+    def test_generate_list_version_4(self):
+        assert hard_coded.generate_list_version_4() == [2, 4, 6, 8, 10]
